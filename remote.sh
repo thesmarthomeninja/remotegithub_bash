da1e919eaa41eb500bf8970e4e53c1f73c213b6a
Chip Oglesby <chip@creativesparkcolumbia.com>
2017-03-07 10:49:52 -0700

    Create remote.sh

---
+++ remote.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Variables
+token='Github Token'
+url='https://api.github.com/repos/chipoglesby/bashAndGithub/contents/test.sh'
+
+# Command To Run
+bash <(curl -H "Content-Type: application/json" -H "Authorization: token $token" $url | jq '.content' | sed -e 's/\\n/ /g' -re 's/"//g' | base64 --decode)
