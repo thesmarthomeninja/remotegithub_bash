a1f5937f47a16aa4842d5ac576d6cf3b8af7eb32
Chip Oglesby <chip@creativesparkcolumbia.com>
2017-03-07 10:50:25 -0700

    Update remote.sh

--- remote.sh
+++ remote.sh
@@ -2,7 +2,7 @@
 
 # Variables
 token='Github Token'
-url='https://api.github.com/repos/chipoglesby/bashAndGithub/contents/test.sh'
+url='https://api.github.com/repos/chipoglesby/bashAndGithub/contents/command.sh'
 
 # Command To Run
 bash <(curl -H "Content-Type: application/json" -H "Authorization: token $token" $url | jq '.content' | sed -e 's/\\n/ /g' -re 's/"//g' | base64 --decode)
