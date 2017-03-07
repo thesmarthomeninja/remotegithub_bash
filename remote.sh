92438f3bac1b6e2eccb021d5f7475afe66856141
Chip Oglesby <chip@creativesparkcolumbia.com>
2017-03-07 11:16:06 -0700

    Update remote.sh

--- remote.sh
+++ remote.sh
@@ -3,8 +3,11 @@
 # Variables
 token='Github Token'
 
-# Update this with your own repo
-url='https://api.github.com/repos/chipoglesby/bashAndGithub/contents/command.sh'
+# Update this with your own info
+owner='Your github username'
+repo='The name of your repo'
+fileName='The name of your file'
+url='https://api.github.com/repos/$owner/$repo/contents/$fileName'
 
 # Command To Run
 bash <(curl -H "Content-Type: application/json" -H "Authorization: token $token" $url | jq '.content' | sed -e 's/\\n/ /g' -re 's/"//g' | base64 --decode)
